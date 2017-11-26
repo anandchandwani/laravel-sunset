a184d64ed88fb578231fae482a0356adce722817
ilia <ilia@uui.ventures>
2017-11-27 01:47:33 +0200

    is_blacklisted filter 20

--- resources/views/editable.blade.php
+++ resources/views/editable.blade.php
@@ -1,7 +1,7 @@
 @component('base')
     @include('apps-table', ['apps' => $apps])
     <hr>
-    @include('ip-table', ['ips' => $ips])
+    @include('ip-table', ['ips' => $ips, 'apps' => $apps])
     <hr>
     @include('requests-table', ['requests' => $requests])
 @endcomponent
