61dcb5009edee7e87ea36c139928bbd2ad4f0abe
ilia <ilia@uui.ventures>
2017-11-27 00:34:13 +0200

    is_blacklisted filter 15

--- app/Http/Controllers/IPController.php
+++ app/Http/Controllers/IPController.php
@@ -25,22 +25,22 @@ class IPController extends Controller
         return;
     }
 
-    public function all(Request $request){
-        $sql = app('db')->table($this->table);
-        $clauses = [];
-        if ($ip = $request->input('ip')) {
-            $clauses[] = ['ip', 'like', $ip . '%'];
-        }
-        if ($os = $request->input('os')) {
-            $clauses[] = ['os', 'like', $os . '%'];
-        }
-//        $country = $request->input('country');
-//        $campaign_id = $request->input('campaign_id');
-//        $is_blacklisted = $request->input('is_blacklisted');
-//        $redirect_url = $request->input('redirect_url');
-
-
-
-        return $sql->where($clauses)->get();
-    }
+//    public function all(Request $request){
+//        $sql = app('db')->table($this->table);
+//        $clauses = [];
+//        if ($ip = $request->input('ip')) {
+//            $clauses[] = ['ip', 'like', $ip . '%'];
+//        }
+//        if ($os = $request->input('os')) {
+//            $clauses[] = ['os', 'like', $os . '%'];
+//        }
+////        $country = $request->input('country');
+////        $campaign_id = $request->input('campaign_id');
+////        $is_blacklisted = $request->input('is_blacklisted');
+////        $redirect_url = $request->input('redirect_url');
+//
+//
+//
+//        return $sql->where($clauses)->get();
+//    }
 }
