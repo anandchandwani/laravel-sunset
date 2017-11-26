2e847324863580cd80b33c3d6ffd3292300aa891
ilia <ilia@uui.ventures>
2017-11-26 17:49:43 +0200

    load ips table via ajax

--- resources/views/ip-table.blade.php
+++ resources/views/ip-table.blade.php
@@ -22,26 +22,29 @@ data-editable-url="/darkcloud/api/ip/">
         data-editable-source="[{value: 0, text: 'No'}, {value: 1, text: 'Yes'}]"
         >is_blacklisted</th>
         <th data-field="redirect_url" data-editable="true">redirect_url</th>
-        <th data-field="state" data-checkbox="true"></th>
+        {{--<th data-field="state" data-checkbox="true"></th>--}}
     </thead>
-    <tbody>
-        @foreach ($ips as $item)
-        <tr>
-            <td>{{$item->id}}</td>
-            <td>{{$item->ip}}</td>
-            <td>
-                <a href="#" data-name="is_blacklisted" data-type="select" data-pk="{{$item->id}}" data-url="/darkcloud/api/ip/" data-value="{{$item->is_blacklisted ? 1 : 0}}"
-                    data-source="[{value: 0, text: 'No'}, {value: 1, text: 'Yes'}]">
-                </a>
-            </td>
+    {{--<tbody>--}}
+        {{--@foreach ($ips as $item)--}}
+        {{--<tr>--}}
+            {{--<td>{{$item->id}}</td>--}}
+            {{--<td>{{$item->ip}}</td>--}}
+            {{--<td>--}}
+                {{--<a href="#" data-name="is_blacklisted" data-type="select" data-pk="{{$item->id}}" data-url="/darkcloud/api/ip/" data-value="{{$item->is_blacklisted ? 1 : 0}}"--}}
+                    {{--data-source="[{value: 0, text: 'No'}, {value: 1, text: 'Yes'}]">--}}
+                {{--</a>--}}
+            {{--</td>--}}
 
-            <td>
-                <a href="#" data-type="text" data-name="redirect_url" data-pk="{{$item->id}}" data-url="/darkcloud/api/ip/" data-title="Update redirect_url, change where the ip will be redirected to in the future.">{{$item->redirect_url}}</a>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
+            {{--<td>--}}
+                {{--<a href="#" data-type="text" data-name="redirect_url" data-pk="{{$item->id}}" data-url="/darkcloud/api/ip/" data-title="Update redirect_url, change where the ip will be redirected to in the future.">{{$item->redirect_url}}</a>--}}
+            {{--</td>--}}
+        {{--</tr>--}}
+        {{--@endforeach--}}
+    {{--</tbody>--}}
 </table>
+
+<br>
+
 <div class="btn-group" role="group" aria-label="...">
     <button type="submit" name="app" value="create" class="btn btn-default btn-danger delete-apps">
         <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
